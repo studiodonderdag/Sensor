0e54635ce3ad60305f3b0ffddaf839bd596c8f0c
Sietze Mulder <info@studiodonderdag.nl>
2012-10-04 19:26:13 +0200

    Carousel met view

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,49 +1,19 @@
-Ext.define("SenSor.view.Main", {
-    extend: 'Ext.tab.Panel',
-    requires: [
-        'Ext.TitleBar',
-        'Ext.Video'
-    ],
-    config: {
-        tabBarPosition: 'bottom',
+Ext.create('Ext.Carousel', {
+    fullscreen: true,
+   // direction: 'vertical',
 
-        items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
+   // defaults: {
+   //     styleHtmlContent: true
+   // },
 
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
-            }
-        ]
-    }
+    items: [
+        {
+            html : 'Nieuws',
+            style: 'background-color: #759E60'
+        },
+        {
+            html : 'Fotos',
+            style: 'background-color: #5E99CC'
+        }
+    ]
 });
